89f484f9e6e8fd757ca57a2c72b11652559ed500
BesarZh <besar.zhubi@student.cacttus.education>
2019-03-22 19:35:59 +0100

    funksionet per ndarje dhe konkatenim

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Main{
   public static void main(String arg[]){
 
@@ -36,29 +38,69 @@ public class Main{
         return permuted;
     };
  
-    public static byte[] byteShift(byte[] arr,int a){
+    public static byte[] byteShift(byte[] arr,int a)
+    {
           byte temp1 = arr[0];
           byte temp2 = arr[1];
           byte[] shiftedArr = new byte[28];
 
-          for(int i=0;i<27;i++) {
-              if(a ==1) {
+          for(int i=0;i<27;i++) 
+          {
+              if(a ==1) 
+              {
                   shiftedArr[i] = arr[i+1];
-              }else if(a ==2) {
+              }else if(a ==2) 
+              {
                   if(i ==26 ) continue;
                   shiftedArr[i] = arr[i+2];
               }
 
           }
-          if(a ==1) {
+          if(a ==1) 
+          {
               shiftedArr[27] = temp1;
-          }else {
+          }else 
+          {
               shiftedArr[26] = temp1;
               shiftedArr[27] = temp2;
           }
 
           return shiftedArr;
-      } 
+      }
+
+      public static HashMap<String, byte[]> halve(byte[] arr)
+    {
+        HashMap<String, byte[]> halves = new HashMap<>(); 
+        byte[] left = new byte[28];
+        byte[] right = new byte[28];
+        
+        for(int i=0;i<28;i++)
+        {
+            left[i]=arr[i];
+            right[i]=arr[i+28];
+        }
+        halves.put("left",left);
+        halves.put("right", right);
+        
+        return halves;
+    }; 
+
+    
+    public static byte[] concatFunc(byte[] left, byte[]right) 
+    {
+
+        byte[]concat = new byte[56];
+        for(int i=0;i<56;i++)
+        {
+            if(i<28)
+                concat[i]=left[i];
+            else
+                concat[i]=right[i-28];
+        }
+        return concat;
+    }
+
+     
 
   }
 }
