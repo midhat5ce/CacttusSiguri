bfe5ecc5336b53ce44526f4231e77af623590edf
BesarZh <besar.zhubi@student.cacttus.education>
2019-03-24 10:28:47 +0100

    Shtimi i klasave permutations dhe shifts

---
+++ Shifts.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+
+import java.util.HashMap;
+
+public class Shifts {
+    
+    
+    public static HashMap<String, byte[]> halve(byte[] arr)
+    {
+        HashMap<String, byte[]> halves = new HashMap<>(); 
+        byte[] left = new byte[28];
+        byte[] right = new byte[28];
+        
+        for(int i=0;i<28;i++)
+        {
+            left[i]=arr[i];
+            right[i]=arr[i+28];
+        }
+        halves.put("left",left);
+        halves.put("right", right);
+        
+        return halves;
+    }; 
+    
+    
+public static byte[] byteShift(byte[] arr,int a){
+    byte temp1 = arr[0];
+    byte temp2 = arr[1];
+    byte[] shiftedArr = new byte[28];
+
+    for(int i=0;i<27;i++) {
+        if(a ==1) {
+            shiftedArr[i] = arr[i+1];
+        }else if(a ==2) {
+            if(i ==26 ) continue;
+            shiftedArr[i] = arr[i+2];
+        }
+
+    }
+    if(a ==1) {
+        shiftedArr[27] = temp1;
+    }else {
+        shiftedArr[26] = temp1;
+        shiftedArr[27] = temp2;
+    }
+
+    return shiftedArr;
+}   
+public static byte[] concatFunc(byte[] left, byte[]right) 
+{
+
+    byte[]concat = new byte[56];
+    for(int i=0;i<56;i++)
+    {
+        if(i<28)
+            concat[i]=left[i];
+        else
+            concat[i]=right[i-28];
+    }
+    return concat;
+}
+
+    
+}
